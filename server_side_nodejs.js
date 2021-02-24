17428ea90ab702ce76bae72b998c2f10aac94d1f
dongzoolee <nant0313@gmail.com>
2021-02-24 15:53:32 +0900

    Update server_side_nodejs.js

--- server_side_nodejs.js
+++ server_side_nodejs.js
@@ -1,30 +1,19 @@
-var express = require('express');
-var app = express();
-var client = require('cheerio-httpcli');
-var https = require('https');
-var fs=require('fs');
-const { countReset } = require('console');
-try {
-    var options = {
-        cert: fs.readFileSync('fullchain.pem'),
-        key: fs.readFileSync('privkey.pem')
-    };
-    var server = https.createServer(options, app)
-        .listen(3338, () => {
-            console.log('server has started on port 3338')
-        });
-} catch (err) {
+const express = require('express');
+const app = express();
+const client = require('cheerio-httpcli');
 
-}
+const server = app.listen(3338, () => {
+        console.log('server has started on port 3338')
+    });
 
 app.use('/sub', (req, res) => {
     var stat = req.query.stat;
-    stat=encodeURIComponent(stat);
-    var url = "http://swopenapi.seoul.go.kr/api/subway/YOUR_API_KEY/xml/realtimeStationArrival/0/1000/"+stat;
-    console.log(url)
+    stat = encodeURIComponent(stat);
+    var url = "http://swopenapi.seoul.go.kr/api/subway/46444b44426e616e383375784f696c/xml/realtimeStationArrival/0/1000/" + stat;
+    // console.log(url)
     client.fetch(url, {}, (err, $, result) => {
-        console.log($.html());
-        res.header("Access-Control-Allow-Origin", "YOUR_SITE");
+        // console.log($.html());
+        res.header("Access-Control-Allow-Origin", "https://leed.at");
         res.end($.html());
     })
 })
